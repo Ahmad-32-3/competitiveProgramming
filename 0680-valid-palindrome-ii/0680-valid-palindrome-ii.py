60970b06ed3c5ee2a8c67c7fff5d6389105e408d
Ahmad-32-3 <muhammadahmad436654@gmail.com>
2025-10-31 16:23:02 -0400

    Time: 27 ms (95.99%), Space: 18 MB (35.54%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if s[l] == s[r]:
+                l += 1
+                r -= 1
+            else:
+                skip_left = s[l+1:r+1]
+                skip_right = s[l:r]
+                return skip_left == skip_left[::-1] or skip_right == skip_right[::-1]
+
+        return True
+                
