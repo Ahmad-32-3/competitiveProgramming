2fdd17e319e767f5eff5d0bb3877016c4cd48120
Ahmad-32-3 <muhammadahmad436654@gmail.com>
2025-11-04 14:31:13 -0500

    Time: 123 ms (54.8%), Space: 17.9 MB (86.76%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,16 +3,15 @@ class Solution:
         count = collections.defaultdict(int)
         l = 0
         res = 0
+        oak = 0
 
         for r in range(len(s)):
             count[s[r]] += 1
+            oak = max(oak, count[s[r]])
 
-            while sum(count.values()) - max(count.values()) > k:
+            while (r - l + 1) - oak > k:
                 count[s[l]] -= 1
-
-                if count[s[l]] == 0:
-                    del count[s[l]]
                 l += 1
             
-            res = max (r - l + 1, res)
+            res = max(r - l + 1, res)
         return res
