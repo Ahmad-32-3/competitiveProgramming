b0d21a0e892c9bf1fe24c325633888f25a9e38a3
Ahmad-32-3 <muhammadahmad436654@gmail.com>
2025-11-11 18:49:27 -0500

    Time: 127 ms (28.54%), Space: 19.8 MB (88.09%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,43 +1,31 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        q = collections.deque()
+        from collections import deque
+        
+        rows, cols = len(grid), len(grid[0])
         directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        visited = set()
-        rowLen = len(grid)
-        colLen = len(grid[0])
-        res = 0
-
-        def bfs(i,j) -> int:
-            q.append((i,j))
-            visited.add((i,j))
-            counter = 0
-            oak = False
-
+        
+        def bfs(i, j):
+            q = deque([(i, j)])
+            grid[i][j] = 0  # mark visited
+            count = 1
+            touches_border = (i == 0 or j == 0 or i == rows-1 or j == cols-1)
+            
             while q:
                 r, c = q.popleft()
-                if r == rowLen - 1 or r == 0 or c == colLen - 1 or c == 0:
-                    oak = True
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-
-                    if 0 <= nr < rowLen and 0 <= nc < colLen and ((nr, nc)) not in visited and grid[nr][nc] == 1:
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        grid[nr][nc] = 0
                         q.append((nr, nc))
-                        visited.add((nr, nc))    
-                if not oak:
-                    counter += 1
-
-            if not oak:
-                return counter 
-            else:
-                return 0
-                        
-
-        for i in range(rowLen):
-            for j in range(colLen):
-                if grid[i][j] == 1 and ((i,j)) not in visited:
-                    res += bfs(i,j)
-        return res
-                
-
-
-
+                        count += 1
+                        if nr == 0 or nr == rows-1 or nc == 0 or nc == cols-1:
+                            touches_border = True
+            return 0 if touches_border else count
+        
+        enclaves = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    enclaves += bfs(i, j)
+        return enclaves
