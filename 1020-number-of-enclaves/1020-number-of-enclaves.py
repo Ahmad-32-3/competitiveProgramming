3d01199b5f9f5ccf3c9be4a95530745bb204f562
Ahmad-32-3 <muhammadahmad436654@gmail.com>
2025-11-11 18:45:33 -0500

    Time: 427 ms (5.08%), Space: 26 MB (67.37%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,46 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        q = collections.deque()
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        visited = set()
+        rowLen = len(grid)
+        colLen = len(grid[0])
+        res = 0
+
+        def bfs(i,j) -> int:
+            q.append((i,j))
+            visited.add((i,j))
+            counter = 0
+            oak = False
+
+            while q:
+                r, c = q.popleft()
+                print(r,c)
+                if r == rowLen - 1 or r == 0 or c == colLen - 1 or c == 0:
+                    oak = True
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+
+                    if 0 <= nr < rowLen and 0 <= nc < colLen and ((nr, nc)) not in visited and grid[nr][nc] == 1:
+                        q.append((nr, nc))
+                        visited.add((nr, nc))
+                        
+                if not oak:
+                    counter += 1
+                print(oak)
+
+            if not oak:
+                return counter 
+            else:
+                return 0
+                        
+
+        for i in range(rowLen):
+            for j in range(colLen):
+                if grid[i][j] == 1 and ((i,j)) not in visited:
+                    res += bfs(i,j)
+        return res
+                
+
+
+
