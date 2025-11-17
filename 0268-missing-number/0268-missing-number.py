d9527aa70c0173c4de72e3ce64142a9be633b368
Ahmad-32-3 <muhammadahmad436654@gmail.com>
2025-11-17 01:30:25 -0500

    Time: 7 ms (37.14%), Space: 19.2 MB (7.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -6,14 +6,7 @@ class Solution:
 
         for i in range(n):
             visited.add(nums[i])
-        
-        # [3,0,1]
-        # = 3
-        # while runSum != n:
-        # if runSum + 1 in visited
-        #     runSum += 1
-        # else: 
-        #      return runSum + 1
+ 
         for i in range(n):
             if nums[i] == 0:
                 while runSum != n:
