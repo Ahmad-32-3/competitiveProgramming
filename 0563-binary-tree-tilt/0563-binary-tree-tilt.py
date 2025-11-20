31d4766bdd8756b2a3b22c6b0508f9f7958ed484
Ahmad-32-3 <muhammadahmad436654@gmail.com>
2025-11-20 18:11:39 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        def dfs(node):
+            nonlocal res
+            if not node:
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            diff = abs(right - left)
+            res += diff
+            return diff
+        return dfs(root)
