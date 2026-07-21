31106faf4237fd92fd3a9b6a2c86a3999e954c61
Ahmad-32-3 <muhammadahmad436654@gmail.com>
2026-07-20 20:18:30 -0400

    Time: 8 ms (49.38%) | Memory: 19.7 MB (52.60%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        word = ""
+        for c in s:
+            if c.isalnum():
+                word += c.lower()
+        
+        if word == word[::-1]:
+            return True
+        return False
+        
