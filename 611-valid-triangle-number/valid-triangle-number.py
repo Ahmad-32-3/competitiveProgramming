ff32ee92a4512d8e2325275fb2d8b6b8e4b1a8f3
Ahmad-32-3 <muhammadahmad436654@gmail.com>
2026-07-28 16:12:15 -0400

    Time: 474 ms (26.66%) | Memory: 19.3 MB (76.18%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -1,23 +1,24 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        # a + b > c
-        # a + c > b
-        # b + c > a
+        # a + b > c : triangle
+        # a <= b <= c
+        # compare a and b with c and then make a conclusion
 
         nums.sort()
         res = 0
+        # [1,2,3,4,4,5,6,8,9]
 
         for i in range(len(nums) - 1, -1, -1):
-            l, r = 0, i - 1
-            
             c = nums[i]
+
+            l, r = 0, i - 1
+
             while l < r:
                 a, b = nums[l], nums[r]
-
-                if a + b <= c:
-                    l += 1
-                else:
-                    res += r - l 
+                if a + b > c:
+                    # all values of a such that a <= b are valid, meaning window from a to b is valid
+                    res += r - l
                     r -= 1
-        return res  
-            
+                else:
+                    l += 1
+        return res
